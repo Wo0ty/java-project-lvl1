72c5f270c457a41b37386390d7915481f5a6c955
Wo0ty <pautov.e@yandex.ru>
2022-03-13 22:26:50 +0300

    add makefile and user greeting

---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void askName() {
+        System.out.println("May I have your name?");
+        
+        Scanner scan = new Scanner(System.in);
+        String userName = scan.nextLine();
+
+        System.out.println("Hello, " + userName + "!");
+    }
+}
